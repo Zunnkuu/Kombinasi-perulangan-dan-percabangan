5c4f166e8b3cc53bcead253a94af643af68ff4d8
Zunnkuu <168741811+Zunnkuu@users.noreply.github.com>
2024-06-10 13:46:48 +0700

    Update 8.go

--- 8.go
+++ 8.go
@@ -4,44 +4,22 @@ import (
 	"fmt"
 )
 
-// Fungsi untuk mengecek apakah suatu bilangan adalah bilangan prima atau tidak
-func isPrime(n int) bool {
-	// 2 adalah bilangan prima
-	if n == 2 {
-		return false
-	}
-
-	// 0 dan 1 bukan bilangan prima
-	if n <= 1 {
-		return false
-	}
-
-	// Jika n adalah bilangan kuadrat dari sebuah bilangan bulat, maka bukan bilangan prima
-	if n > 2 && n%2 == 0 {
-		return false
-	}
-
-	// Iterasi dari 3 hingga akar dari n
-	for i := 3; i*i <= n; i += 2 {
-		// Jika n habis dibagi i, maka bukan bilangan prima
+func isPrimeX(n int) bool {
+	factors := 0
+	for i := 1; i <= n; i++ {
 		if n%i == 0 {
+			factors++
+		}
+		if factors > 3 {
 			return false
 		}
 	}
-
-	// Jika tidak ditemukan pembagi selain 1 dan n itu sendiri, maka bilangan prima
-	return true
+	return factors == 3
 }
 
 func main() {
 	var num int
-	fmt.Scanln(&num)
 
-	// Untuk bilangan 4 dan 9, karena memiliki 3 faktor (1, bilangan itu sendiri, dan akar dari bilangan itu sendiri),
-	// maka dalam konteks definisi di planet X, keduanya dianggap sebagai bilangan prima
-	if num == 4 || num == 9 {
-		fmt.Println(true)
-	} else {
-		fmt.Println(isPrime(num))
-	}
+	fmt.Scan(&num)
+	fmt.Println(isPrimeX(num))
 }
